9eab008d9acde95da04a25a7766c0a391db2c510
Sobhan Tahmasebi <niinja2017@gmail.com>
2026-01-31 16:36:19 +0330

    Example one End Array Method

---
+++ Example one/script.js
@@ -0,0 +1,112 @@
+/////////////////////////////////////////// list1
+// let list1 = ['apple', 'banana']
+
+// list1.pop()
+// console.log(list1)
+// list1.push('orange')
+// console.log(list1)
+
+
+/////////////////////////////////////////// list2
+// let list2 = ['Ali' , 'Sara' , 'Reza'];
+
+// list2.shift()
+// console.log(list2)
+// list2.unshift('mohammad')
+// console.log(list2)
+
+/////////////////////////////////////////// list3
+// let list3 = ['1' , '2' , '3' , '4']
+// console.log(list3[list3.length - 1])
+
+
+
+/////////////////////////////////////////// list4
+// let list4 = [100,200,300]
+
+// let result1 = list4.map(item => {
+//     return item * 1.2
+// })
+// console.log(result1)
+
+/////////////////////////////////////////// list5
+// let list5 = [2, 7, 4, 9, 12, 5];
+// let result2 = list5.filter(item => {
+//     return item % 2 == 0
+// })
+
+// let result3 = list5.filter(item => {
+//     return item > 5
+// })
+
+// console.log(result2 , result3)
+
+/////////////////////////////////////////// list6
+// let list6 = ['admin', 'user', 'editor'];
+// if(list6.includes('guest')){
+//     console.log(true)
+// } else {
+//     list6.push('guest')
+//     console.log(list6)
+// }
+
+/////////////////////////////////////////// list7
+// let list7 = ['JavaScript', 'is', 'awesome'];
+
+// let result4 = list7.join('-')
+// console.log(result4)
+
+
+/////////////////////////////////////////// list8
+// let list8 = [10, 18, 15, 20];
+
+// let result5 = list8.reduce((sum, total) => {
+//     return sum + total
+// }, 0) / list8.length
+
+// let result6 = list8.reduce((max, total) => {
+//     return total > max ? total : max
+// })
+
+// console.log(result5, result6)
+
+
+/////////////////////////////////////////// list9
+// let list9 = [25, 5, 18, 40, 12];
+// list9.sort((a,b) => a - b)
+// console.log(list9)
+
+
+/////////////////////////////////////////// list10
+// let list10 = [
+//     { name: "Ali", age: 22 },
+//     { name: "Sara", age: 17 },
+//     { name: "Reza", age: 30 }
+// ]
+
+// let result7 = list10.filter(item => {
+//     return item.age > 18
+// })
+
+// let result8 = result7.map(item => {
+//     return item.name
+// })
+
+// let result9 = result8.join(',')
+
+// let result10 = list10.reduce((sum , total) => {
+//     return sum + total.age
+// } , 0)
+
+// console.log(result7 , result8 ,  result9 , result10)
+
+/////////////////////////////////////////// list10
+// let list11 = [
+//   { name: "Ali", age: 22 },
+//   { name: "Sara", age: 17 },
+//   { name: "Reza", age: 30 }
+// ]
+
+// list11.sort((a, b) => a.age - b.age)
+
+// console.log(list11)
